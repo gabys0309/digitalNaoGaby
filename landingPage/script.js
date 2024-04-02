c8a6b45b1d982a86c83e77eb5786b136c968bf87
gabys0309 <109407222+gabys0309@users.noreply.github.com>
2024-04-02 11:52:51 -0600

    Limpieza y documentación del código

--- landingPage/script.js
+++ landingPage/script.js
@@ -1,13 +1,1 @@
-// Smooth scroll para los enlaces de navegación
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
 
-        const target = document.querySelector(this.getAttribute('href'));
-
-        window.scrollTo({
-            top: target.offsetTop,
-            behavior: 'smooth'
-        });
-    });
-});
